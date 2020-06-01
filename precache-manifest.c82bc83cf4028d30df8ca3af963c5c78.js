4b78e519b4f4f732c31f69e8fc2a689f4175d810
Jiachen <zhangjiachen6@gmail.com>
2020-05-31 22:47:13 -0700

    Updates

---
+++ precache-manifest.c82bc83cf4028d30df8ca3af963c5c78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2cb627d00934706e847989671148f2",
+    "url": "/cs185_movie/index.html"
+  },
+  {
+    "revision": "10d0272084d4522526ef",
+    "url": "/cs185_movie/static/css/main.5ad24ddf.chunk.css"
+  },
+  {
+    "revision": "9d7f8646be134c57f921",
+    "url": "/cs185_movie/static/js/2.4ec857de.chunk.js"
+  },
+  {
+    "revision": "a855a7222d00a33eb6846ce89bcfdce5",
+    "url": "/cs185_movie/static/js/2.4ec857de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d0272084d4522526ef",
+    "url": "/cs185_movie/static/js/main.27ca5a26.chunk.js"
+  },
+  {
+    "revision": "e95fe60e6da9c8eca7de",
+    "url": "/cs185_movie/static/js/runtime-main.8bdad75b.js"
+  }
+]);
